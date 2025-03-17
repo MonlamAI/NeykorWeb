95149dd14efdf235c52228286f059978516f3fa3
TenzDelek <tibetdelek@gmail.com>
2025-03-17 15:45:45 +0530

    remove shadcn use

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -8,10 +8,13 @@ export default async function AdminLayout({
   children: React.ReactNode;
 }) {
   const session = await getSession();
+  console.log(session)
   if (!session?.user) {
+    console.log("No session, redirecting to login");
     redirect('/api/auth/login?returnTo=/admin');
   }
 
+  console.log("User role:", session.user.role); 
   if (session.user.role !== 'ADMIN') {
     redirect('/');
   }
